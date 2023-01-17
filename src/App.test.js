f7686544b363dc725b42b8c41885288f75904a5c
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-17 15:31:11 -0800

    forgot to remove .skip from App test!

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test.skip('fetches the list of blogs', async () => {
+test('fetches the list of blogs', async () => {
   render(<App />);
   const blog = await screen.findByText(/Maximize Integrated Networks/i);
   expect(blog).toBeInTheDocument();
