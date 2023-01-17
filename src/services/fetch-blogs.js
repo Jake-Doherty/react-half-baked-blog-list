46f6bd1e86552c08ce6e1525422a083a812ebbca
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-17 14:32:41 -0800

    gets author name by fkey + author name to BlogCard

--- src/services/fetch-blogs.js
+++ src/services/fetch-blogs.js
@@ -1,6 +1,6 @@
 import { checkError, client } from './client.js';
 
 export async function fetchAllBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select('*, authors(name)');
   return checkError(resp);
 }
